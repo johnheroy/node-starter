70a700617036370c6aec990b933522b98b05bff5
John Heroy <johnheroy@gmail.com>
2018-11-24 23:45:40 -0800

    Hacky workaround for crash issue

--- src/hello/HelloController.ts
+++ src/hello/HelloController.ts
@@ -1,8 +1,9 @@
-import { controller, httpGet } from 'inversify-express-utils'
+import { controller, httpGet, response, request, queryParam } from 'inversify-express-utils'
 import { Request, Response } from 'express'
 import { Hello } from '../entity/Hello'
 import { getRepository, Repository } from 'typeorm'
 import { from } from 'rxjs'
+import { ignoreElements } from 'rxjs/operators'
 
 @controller('/hello')
 export class HelloController {
@@ -10,25 +11,33 @@ export class HelloController {
   private repository: Repository<Hello> = getRepository(Hello)
 
   @httpGet('/')
-  public home(req: Request, res: Response) {
-    let name = req.query.name || 'World'
+  public home(
+      @request() req: Request, 
+      @response() res: Response,
+      @queryParam('name') name: string): Promise<void> {
+
+    name = name || 'World'
     let user = this.repository.create({ name: name })
 
-    from(this.repository.insert(user))
-      .subscribe((hello) => {
-        res.status(200).end()
-      }, (error) => {
-        res.status(500).end()
-      })
+    let observable = from(this.repository.insert(user))
+    observable.subscribe((hello) => {
+      res.send(`Hello ${name}`)
+    }, (error) => {
+      res.status(500).end()
+    })
+
+    return observable.pipe(ignoreElements()).toPromise()
   }
 
   @httpGet('/all')
-  public all(req: Request, res: Response) {
-    res.status(200).end()
-    from(this.repository.find())
-      .subscribe((hellos: Hello[]) => { 
-        console.log(JSON.stringify(hellos))
-      })
+  public all(req: Request, res: Response): Promise<void> {
+    let observable = from(this.repository.find())
+    observable.subscribe((hellos: Hello[]) => { 
+      res.json(hellos)
+    }, (error) => {
+      res.status(500).end()
+    })
+
+    return observable.pipe(ignoreElements()).toPromise()
   }
 }
-
